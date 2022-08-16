2be66229689c90641f28eaa181738e02735b0256
DieggsPapuM <103551254+DieggsPapuM@users.noreply.github.com>
2022-08-16 11:32:36 -0700

    Lab4copy = lab5

--- Lab4 - Copy/app/src/main/java/com/example/lab4/DetailsActivity.kt
+++ Lab4 - Copy/app/src/main/java/com/example/lab4/DetailsActivity.kt
@@ -2,10 +2,27 @@ package com.example.lab4
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class DetailsActivity : AppCompatActivity() {
+    lateinit var btnInitV : Button
+    lateinit var txtVCall: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_details)
+        btnInitV = findViewById(R.id.btn_detailsActivity_IniciarVista)
+
+        txtVCall = findViewById(R.id.txtV_detailsActivity_llamar)
+        initListeners()
+    }
+    private fun initListeners(){
+        btnInitV.setOnClickListener {
+            
+        }
+        txtVCall.setOnClickListener {
+
+        }
     }
 }
---
+++ Lab4 - Copy/app/src/main/java/com/example/lab4/MainActivity.kt
@@ -0,0 +1,55 @@
+package com.example.lab4
+import android.content.ActivityNotFoundException
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import android.net.Uri
+import android.widget.ImageButton
+
+class MainActivity : AppCompatActivity() {
+    lateinit var  buttonToasterFullName: Button
+    lateinit var buttonTerminarJornada : Button
+    lateinit var iconDirection : ImageButton
+    lateinit var buttonDownload : Button
+    lateinit var buttonDetails : Button
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        buttonToasterFullName = findViewById<Button>(R.id.btn_mainActivity_cardV_Iniciar)
+        buttonTerminarJornada = findViewById<Button>(R.id.btn_mainActivity_FinishJ)
+        buttonDownload = findViewById<Button>(R.id.btnDownload_mainActivity_download)
+        iconDirection = findViewById(R.id.Ibtn_mainActivity_cardV_Restaurant)
+        buttonDetails = findViewById<Button>(R.id.button_mainActivity_details)
+        initListeners()
+    }
+
+    private fun initListeners(){
+        buttonToasterFullName.setOnClickListener{
+            Toast.makeText(this, "Diego Andres Alonzo Medinilla", Toast.LENGTH_LONG).show()
+        }
+        buttonDownload.setOnClickListener {
+            try {
+                val intent = Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id=$packageName"))
+                startActivity(intent)
+            } catch (e: ActivityNotFoundException) {
+                val intent = Intent(Intent.ACTION_VIEW, Uri.parse("https://play.google.com/store/apps/details?id=$packageName"))
+                startActivity(intent)
+            }
+        }
+        iconDirection.setOnClickListener {
+            val location = "http://maps.google.com/maps?q=loc:14.603835110728907,-90.48925677205315"
+            val intent = Intent(Intent.ACTION_VIEW, Uri.parse(location))
+            startActivity(intent)
+        }
+        buttonDetails.setOnClickListener {
+            val intent = Intent(this, DetailsActivity::class.java)
+            intent.addFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
+            startActivity(intent)
+        }
+
+
+
+    }
+}
