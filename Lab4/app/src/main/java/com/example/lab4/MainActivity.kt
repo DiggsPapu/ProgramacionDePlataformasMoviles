c87e8b1d1b8c381609e1e5caa31973ff8c6f1b5b
DieggsPapuM <103551254+DieggsPapuM@users.noreply.github.com>
2022-08-28 11:05:15 -0700

    Lab07

--- Lab4/app/src/main/java/com/example/lab4/MainActivity.kt
+++ Lab4/app/src/main/java/com/example/lab4/MainActivity.kt
@@ -1,6 +1,4 @@
 package com.example.lab4
-
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
@@ -9,18 +7,22 @@ import android.widget.Toast
 class MainActivity : AppCompatActivity() {
     lateinit var  buttonToasterFullName: Button
     lateinit var buttonTerminarJornada : Button
-
+    lateinit var iconDirection : Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         buttonToasterFullName = findViewById<Button>(R.id.btn_mainActivity_cardV_Iniciar)
         buttonTerminarJornada = findViewById<Button>(R.id.btn_mainActivity_FinishJ)
+        iconDirection = findViewById(R.id.txtV_mainActivity_CardV_direction)
         initListeners()
     }
 
     private fun initListeners(){
         buttonToasterFullName.setOnClickListener{
             Toast.makeText(this, "Diego Andres Alonzo Medinilla", Toast.LENGTH_LONG).show()
+        }
+
+
         }
     }
 }
