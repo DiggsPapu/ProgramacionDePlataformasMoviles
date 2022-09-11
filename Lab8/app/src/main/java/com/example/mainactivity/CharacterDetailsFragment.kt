e51a15923ad5d7c7a02290f63dad4ea04f10b345
DieggsPapuM <103551254+DieggsPapuM@users.noreply.github.com>
2022-09-11 13:18:47 -0700

    Lab8

---
+++ Lab8/app/src/main/java/com/example/mainactivity/CharacterDetailsFragment.kt
@@ -0,0 +1,6 @@
+package com.example.mainactivity
+
+import androidx.fragment.app.Fragment
+
+class CharacterDetailsFragment: Fragment(R.layout.characterdetails_fragment) {
+}
---
+++ Lab8/app/src/main/java/com/example/mainactivity/CharactersFragment.kt
@@ -0,0 +1,6 @@
+package com.example.mainactivity
+
+import androidx.fragment.app.Fragment
+
+class CharactersFragment: Fragment(R.layout.characters_fragment) {
+}
