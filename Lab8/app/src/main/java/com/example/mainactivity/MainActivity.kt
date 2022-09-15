93aa1acb7c5d12b7a030de53b0f71d8b38523966
DieggsPapuM <103551254+DieggsPapuM@users.noreply.github.com>
2022-09-15 09:57:51 -0700

    Lab08

--- Lab8/app/src/main/java/com/example/mainactivity/MainActivity.kt
+++ Lab8/app/src/main/java/com/example/mainactivity/MainActivity.kt
@@ -2,10 +2,37 @@ package com.example.mainactivity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.appbar.MaterialToolbar
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var toolbar: MaterialToolbar
+    private lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.FragmentContainer_MainAct) as NavHostFragment
+        navController = navHostFragment.navController
+        val appbarconfig = AppBarConfiguration(navController.graph)
+        toolbar = findViewById(R.id.toolbar_MainAct)
+        toolbar.setupWithNavController(navController, appbarconfig)
+        listenToNavGraphChanges()
+    }
+    private fun listenToNavGraphChanges(){
+        navController.addOnDestinationChangedListener(NavController.OnDestinationChangedListener{_, destination,_->
+            when(destination.id){
+                R.id.charactersFragment->{
+                    toolbar.visibility = View.VISIBLE
+                }
+                else -> {
+                    true
+                }
+            }
+        }
+        )
     }
 }
