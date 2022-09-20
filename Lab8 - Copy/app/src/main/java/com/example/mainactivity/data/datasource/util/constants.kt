b6636e71698e91f18bf0a3d1d6c8c55da31e49ca
DieggsPapuM <103551254+DieggsPapuM@users.noreply.github.com>
2022-09-19 23:16:14 -0700

    L9 API

---
+++ Lab8 - Copy/app/src/main/java/com/example/mainactivity/data/datasource/util/constants.kt
@@ -0,0 +1,7 @@
+package com.example.mainactivity.data.datasource.util
+
+class constants {
+    companion object{
+        const val RICKANDMORTY_BASE_URL = "https://rickandmortyapi.com/api/"
+    }
+}
