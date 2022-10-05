4dfd3b015da09dcce298408041756c28cb6277eb
DieggsPapuM <103551254+DieggsPapuM@users.noreply.github.com>
2022-10-05 15:14:45 -0700

    Gradle

---
+++ Lab11/app/src/main/java/com/example/mainactivity/activity/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.mainactivity.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
+import com.example.mainactivity.R
+import com.google.android.material.appbar.MaterialToolbar
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
---
+++ Lab11/app/src/main/java/com/example/mainactivity/data/datasource/model/variouscharacters/AllAssetsForAllResponse.kt
@@ -0,0 +1,6 @@
+package com.example.mainactivity.data.datasource.model.variouscharacters
+
+data class AllAssetsForAllResponse(
+    val info: Info,
+    val results: List<Result>
+)
