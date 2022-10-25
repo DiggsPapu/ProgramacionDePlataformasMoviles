93db52a4437725f344058d4ed3c805ae7117bd98
DieggsPapuM <103551254+DieggsPapuM@users.noreply.github.com>
2022-10-24 20:15:09 -0700

    ViewModelSession

---
+++ Lab12/app/src/main/java/com/example/lab12/ViewModelSession.kt
@@ -0,0 +1,60 @@
+package com.example.lab12
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+
+private const val TOKEN_DURATION = 5000L
+private const val LOGIN_TIME = 3000L
+
+class ViewModelSession : ViewModel(){
+    sealed interface LoginState {
+        object Default: LoginState
+        object Success: LoginState
+        object Loading: LoginState
+        class Error(val message: String): LoginState
+    }
+
+    private val autentificationtoken = MutableStateFlow(false)
+    val validAuthToken: StateFlow<Boolean> = autentificationtoken
+
+    private val statusLogin = MutableStateFlow<LoginState>(LoginState.Default)
+    val loginStatus: StateFlow<LoginState> = statusLogin
+
+    private lateinit var job: Job
+
+
+    fun login(email: String, password: String) {
+        viewModelScope.launch {
+            statusLogin.value = LoginState.Loading
+            delay(LOGIN_TIME)
+            if (email == "jcdurini@uvg.edu.gt" && email == password) {
+                statusLogin.value = LoginState.Success
+            } else {
+                statusLogin.value = LoginState.Error("Credenciales incorrectas")
+            }
+
+            statusLogin.value = LoginState.Default
+        }
+    }
+
+    fun generateToken() {
+        job = viewModelScope.launch {
+            autentificationtoken.value = true
+            delay(TOKEN_DURATION)
+            autentificationtoken.value = false
+        }
+    }
+
+    fun cancelJob() {
+        job.cancel()
+    }
+
+    fun logOut() {
+        job.cancel()
+        autentificationtoken.value = false
+    }
+}
